84a44e83d9a8b69421e4b5f4bfdcaefa52b155f8
byMan <nana279@gmail.com>
2021-07-16 11:02:35 +0900

    .

--- src/algo/그래프/최소공통조상_LCA/Bj_3176_도로네트워크.java
+++ src/algo/그래프/최소공통조상_LCA/Bj_3176_도로네트워크.java
@@ -83,8 +83,8 @@ public class Bj_3176_도로네트워크 {
     }
 
     private static void f() {
-        for (int j = 1; j < 21; j++) {
-            for (int i = 1; i <= n; i++) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j < 21; j++) {
                 par[i][j] = par[par[i][j - 1]][j - 1];
                 qmin[i][j] = Math.min(qmin[i][j - 1], qmin[par[i][j - 1]][j - 1]);
                 qmax[i][j] = Math.max(qmax[i][j - 1], qmax[par[i][j - 1]][j - 1]);
